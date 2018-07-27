c2815ac51b7520089ca259fd569df64eb1a19bc6
Frank <akura11.tt@gmail.com>
2018-07-26 21:12:52 -0500

    Fix: EOPI 81

--- dgm_tools/catalog/urls.py
+++ dgm_tools/catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 from .viewsets import PostViewSet
-from .views import catalog_home, catalog_tool
+from .views import catalog_home, catalog_tool, catalog_filter
 
 
 router = DefaultRouter()
@@ -13,5 +13,7 @@ urlpatterns = [
     url(r'^api/', include(router.urls)),
     path('', catalog_home, name="home"),
     path('p/<int:page>/', catalog_home, name="home_pagination"),
+    path('search/', catalog_filter, name="search_filter"),
+    path('search/<int:page>/', catalog_filter, name="search_filter_pagination"),
     path('herramientas/<slug:slug>/', catalog_tool, name='catalog_post'),
 ]
--- dgm_tools/catalog/views.py
+++ dgm_tools/catalog/views.py
@@ -2,9 +2,9 @@
 Configuracion de vistas del CMS
 Vistas para usuarios de datos.gob.mx
 """
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from .models import Post, Category
 
@@ -49,6 +49,66 @@ def catalog_home(request, page=1):
 
     return render(request, 'catalog.html', context)
 
+
+def catalog_filter(request, page=1):
+    """
+    URL: /soluciones-abiertas/search/p/<int:page>/
+    Vista que lista los posts de herramientas
+    pagina en grupos de 10 posts
+
+    Los posts se ordenan del mas reciente al mas antiguo
+
+    Parametros:
+        - page <int>: Indice de paginacion
+    """
+    import operator
+    from functools import reduce
+    from django.db.models import Q
+
+    title = request.GET.get('title', '').strip()
+    tag = request.GET.get('tipo', '')
+    level = request.GET.get('dificultad', '')
+
+    if not title:
+        return redirect('/soluciones-abiertas/')
+
+    q_list = [Q(public=True), Q(title__unaccent__icontains=title)]
+
+    if tag:
+        q_list.append(Q(tag=tag))
+
+    if level:
+        q_list.append(Q(level=level))
+
+    # Obteniendo posts publicados
+    posts = Post.objects\
+        .prefetch_related('category')\
+        .filter(reduce(operator.and_, q_list))\
+        .order_by('-id')
+    pagination = Paginator(posts, 10)
+
+    # Obteniendo variables de control publicas
+    categories = Category.objects.filter(public=True).order_by('id')
+
+    # Paginacion
+    page_obj = pagination.page(page)
+    p_range = list(pagination.page_range)
+
+    # Paginacion
+    init = 0 if page <= 5 else page - 5
+    end = page_obj.end_index() if page > page_obj.end_index() - 5 else page + 5
+    p_range = p_range[init:end]
+
+    context = {
+        'settings': settings,
+        'posts': page_obj,
+        'categories': categories,
+        'p_range': p_range,
+        'init': init,
+        'end': end
+    }
+
+    return render(request, 'catalog.html', context)
 
 def catalog_tool(request, slug=None):
     """
